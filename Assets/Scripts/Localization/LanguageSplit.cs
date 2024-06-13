8076d206b476179326643f8e8fe7fd556be61518
Zebado <sbasualdo.antares@gmail.com>
2024-06-13 16:08:41 -0300

    1 hora 16 minutos

---
+++ Assets/Scripts/Localization/LanguageSplit.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanguageSplit
+{
+    public static Dictionary<Language, Dictionary<string, string>> LoadCSV(string sheet, string source)
+    {
+        var codex = new Dictionary<Language, Dictionary<string, string>>();
+
+        var langColumn = new Dictionary<int, Language>();
+        int idColumn = 0;
+
+        var lines = sheet.Split(new[] { '\n', '\r' }, System.StringSplitOptions.RemoveEmptyEntries);
+
+        bool firstLine = true;
+
+
+        foreach (var line in lines)
+        {
+            var cells = line.Split(',');
+
+            if (firstLine)
+            {
+                firstLine = false;
+                for (int i = 0; i < cells.Length; i++)
+                {
+                    if (!cells[i].Contains("ID"))
+                    {
+                        try
+                        {
+                            langColumn[i] = (Language)Enum.Parse(typeof(Language), cells[i]);
+                        }
+                        catch(Exception e)
+                        {
+                            Debug.Log($"{e.ToString()} - source: {source}");
+                            continue;
+                        }
+
+                        if (!codex.ContainsKey(langColumn[i]))
+                        {
+                            codex[langColumn[i]] = new Dictionary<string, string>();
+                        }
+                    }
+                    else
+                    {
+                        idColumn = i;
+                    }
+                }
+                continue;
+            }
+
+            for (int i = 0; i < cells.Length; i++)
+            {
+                if (i == idColumn) continue;
+
+                var lang = langColumn[i];
+                var id = cells[idColumn];
+                var textValue = cells[i];
+
+                codex[lang][id] = textValue;
+            }
+        }
+        return codex;
+    }
+}
---
+++ Assets/Scripts/Localization/Localization.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public enum Language
+{
+    Spanish,
+    English
+}
+
+public class Localization : MonoBehaviour
+{
+
+    [SerializeField] string _webURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTasgbIF7L3PGbDRN80dZbeOeBCnAWFLz7sphsxP5RTlzxeybtqHAnV3kJKn4LgDOzByCML-0JVhOp0/pub?output=csv";
+
+    [SerializeField] Language _currentLang;
+
+    Dictionary<Language, Dictionary<string, string>> _languageCodex;
+
+    public event Action onUpdate = delegate { };
+
+    private void Awake()
+    {
+        StartCoroutine(DownloadCSV(_webURL));
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            _currentLang = _currentLang == Language.English ? Language.Spanish : Language.English;
+
+            onUpdate();
+        }
+    }
+
+    IEnumerator DownloadCSV(string url)
+    {
+        var www = new UnityWebRequest(url);
+
+        www.downloadHandler = new DownloadHandlerBuffer();
+
+        yield return www.SendWebRequest();
+
+        var result = www.downloadHandler.text;
+
+        _languageCodex = LanguageSplit.LoadCSV(result, "web");
+    }
+
+    public string GetTranslate(string ID)
+    {
+        var idsDictionary = _languageCodex[_currentLang];
+
+        idsDictionary.TryGetValue(ID, out var result);
+
+        return result;
+    }
+}
