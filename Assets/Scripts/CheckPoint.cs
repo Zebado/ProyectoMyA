f22fb52fac8da85d3cfbac21e6cde7ba494d7735
Zebado <sbasualdo.antares@gmail.com>
2024-06-28 15:12:05 -0300

    UJpdate

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -1,11 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class CheckPoint : MonoBehaviour
 {
     bool _IsTrigger;
-
+    PInputManager _inputManager;
+    private void OnEnable()
+    {
+        _inputManager.OnCheckPoint += CheckPointOn;
+    }
+    private void OnDisable()
+    {
+        _inputManager.OnCheckPoint -= CheckPointOn; 
+    }
+    private void Awake()
+    {
+        _inputManager = FindAnyObjectByType<PInputManager>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
@@ -22,7 +36,7 @@ public class CheckPoint : MonoBehaviour
     }
     public void CheckPointOn()
     {
-        if (_IsTrigger)
+        if (_IsTrigger)     
         {
             GameManager.Instance.SaveCheckPoint();
             Debug.Log("CheckPoint Saved");
